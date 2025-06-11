2a275c2d300100a85b21aad12884cad8723521d9
githubdevelopers123 <44903121+githubdevelopers123@users.noreply.github.com>
2025-06-11 22:33:54 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -3,6 +3,6 @@ exports.hello = async (event) => {
   console.log("Received event:", JSON.stringify(event));
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda!" }),
+    body: JSON.stringify({ message: "Hello from Lambda!Sriman" }),
   };
 };
